da5872c8141c064e747a6cff8bde4671bb634f04
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-08-26 22:49:48 +0530

    PAstel page

---
+++ src/main/java/com/example/pastelpages/PastelPagesApplication.java
@@ -0,0 +1,10 @@
+package com.example.pastelpages;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelPagesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PastelPagesApplication.class, args);
+    }
+}
